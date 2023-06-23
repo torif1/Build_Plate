f8ab36f99b4afd0edf19944b35b3edbe409105d4
2208504 <122637128+torif1@users.noreply.github.com>
2023-06-23 13:51:12 -0600

    Eliminate unused variables from removed function
    All of the variables used to convert from  a given temperature to a resistance have been removed to save space.

--- Thermistor.h
+++ Thermistor.h
@@ -8,11 +8,6 @@ class Thermistor {
 private:
   byte pin;
   byte resistor;
-  long temperachurenomial = 25;   //room temperachure
-  long thermistornomial = 10000;  //thermistor resistance at room temperachure
-
-  //TODO: Check beta cofficient against datasheet.
-  long bcofficient = 3950;  //beta coeffieient of thermistor
 
 public:
   Thermistor(byte pin, byte resistance);
