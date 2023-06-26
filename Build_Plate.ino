af4ccd62dcf44038e3d11d0357ddb1a6dff43c2a
2208504 <122637128+torif1@users.noreply.github.com>
2023-06-26 11:24:54 -0600

    Included thermistor code for testing temperatures
    Defined the thermistor and include temperature sensing in order to monitor Peltier cooler temperature while testing.

--- Build_Plate.ino
+++ Build_Plate.ino
@@ -1,11 +1,14 @@
 #include "Thermistor.h"
+#include "Hbridge.h"
 
-Thermistor thermistor(A0, 10000);  //Created a thermistor for analogue pin A0, with a resistor with resistance 10,000 ohm.
+Thermistor thermistor(A0, 10000);  //created a thermistor for analogue pin A0, with a resistor with resistance 10,000 ohm.
+Hbridge hbridge(3, 5);             //created a hbridge for pwm pins 3 and 5
 
 void setup() {
   Serial.begin(9600);  // Begins Serial monitor for 9600 baud
 }
-
 void loop() {
+  Serial.print(thermistor.retriveresistance());
+  hbridge.coldsurface();
   delay(1000);  //delay in loop
 }
