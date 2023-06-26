a5991587e576b0f33cd1c947bcf2212390781615
2208504 <122637128+torif1@users.noreply.github.com>
2023-06-26 14:28:24 -0600

    If Else implementation of control loop
    Added the simplest, yet least modular version of the closed loop control

--- Build_Plate.ino
+++ Build_Plate.ino
@@ -2,28 +2,39 @@
 #include "Hbridge.h"
 
 #define labresistance 18
-#define goalresistance 27
+#define goal_resistance 27
+
+float lab_resistance;
 
 Thermistor thermistor(A0, 10000);  //created a thermistor for analogue pin A0, with a resistor with resistance 10,000 ohm.
 Hbridge hbridge(3, 5);             //created a hbridge for pwm pins 3 and 5
 
+
+
 void setup() {
   Serial.begin(9600);  // Begins Serial monitor for 9600 baud
+  lab_resistance = thermistor.retriveresistance();  //finds initial temperachure of the lab 
+  
 }
 void loop() {
   Serial.print(thermistor.retriveresistance());
   hbridge.coldsurface();
   delay(1000);  //delay in loop
 
-  //I need to basically check what the temperachure sensor is and then determine wether I need to turn the thermistor on or off based on that temperchure and the goal resistance
+if (goal_resistance<lab_resistance){ //goal temp higher than lab temp
 
-  if goalresistance<labresistance: // goal temp higher than lab temp
-    should be turning on and off heating plate
-  if goalresistacne>labresistance: // goal temp lower than lab temp.
-    turning on and off cooling plate
-
-if (goalresistance<labresistance){. //goal temp higher than lab temp
-run the code to turn on and off the peltier coolers
-
+  if (lab_resistance<goal_resistance) {
+    hbridge.coldsurface();
+  } else {
+    hbridge.off();
+  }
+} else if (goal_resistance>lab_resistance){
+  if (lab_resistance>goal_resistance){
+  hbridge.hotsurface();
+  } else {
+    hbridge.off();
+  }
+} else {
+  hbridge.off();
 }
 }
