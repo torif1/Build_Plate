8a62e1698b479f94a4a9b5831ec8beea994772d5
2208504 <122637128+torif1@users.noreply.github.com>
2023-06-27 10:51:09 -0600

    Formated Document
    Format document to be more readable and legible

--- Build_Plate.ino
+++ Build_Plate.ino
@@ -9,14 +9,11 @@ float lab_resistance;
 Thermistor thermistor(A0, 10000);  //created a thermistor for analogue pin A0, with a resistor with resistance 10,000 ohm.
 Hbridge hbridge(3, 5);             //created a hbridge for pwm pins 3 and 5
 
-
-
 void setup() {
-  Serial.begin(9600);  // Begins Serial monitor for 9600 baud
-  lab_resistance = thermistor.retriveresistance();  //finds initial temperachure of the lab 
-  
+  Serial.begin(9600);                               // Begins Serial monitor for 9600 baud
+  lab_resistance = thermistor.retriveresistance();  //finds initial temperachure of the lab
 }
+
 void loop() {
   delay(1000);  //delay in loop
-
 }
