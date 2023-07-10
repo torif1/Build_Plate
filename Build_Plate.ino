d20b6708760264e32d8f426ebb3339eef151517a
2208504 <122637128+torif1@users.noreply.github.com>
2023-07-10 09:18:32 -0600

    Added PID object
    I added the PID object to the code, with initial tuning parameters.

--- Build_Plate.ino
+++ Build_Plate.ino
@@ -1,13 +1,18 @@
 #include "Thermistor.h"
 #include "Hbridge.h"
+#include <PID_v1.h>
 
 #define goal_resistance 17  // goal temperachure
 
 float global_resistance;      // room temperachure
-float thermistor_resistance;  // resistance read by thermistor
+double thermistor_resistance;  // resistance read by thermistor
+
+double Setpoint, Input, Output; // variables for PID
+
 
 Thermistor thermistor(A0, 10000);  // created a thermistor for analogue pin A0, and resistor with resistance 10,000 ohm.
 Hbridge hbridge(3, 5);             // created a hbridge for pwm pins 3 and 5
+PID mypid(&Input, &thermistor_resistance, &Setpoint, 2, 5, 1, DIRECT);  // created pid loop
 
 void setup() {
   Serial.begin(9600);                                      // Begins serial monitor for 9600 baud
@@ -17,6 +22,8 @@ void setup() {
 void loop() {
 
   thermistor_resistance = thermistor.retriveresistance();  // find thermistor resistance at given time
+  //Input=thermistor_resistance;  // set input to be equal to thermistor resistance
+
 
   // record temperachure in serial monitor
   Serial.print(thermistor_resistance);
